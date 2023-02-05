da0e8255ca090b40c2736b491b7f1dfaec3aaaf1
emirmorgan <aemirdnr@gmail.com>
2023-02-05 04:58:34 +0300

    Update: CreateTodo component

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import CreateTodo from "./components/CreateTodo.vue";
+</script>
+
+<template>
+  <h2 class="todo-text" style="margin-bottom: 24px">Vue To-Do App</h2>
+  <CreateTodo />
+</template>
+
+<style scoped>
+.todo-text {
+  text-align: center;
+  font-size: 3rem;
+  font-weight: 700;
+
+  -webkit-text-stroke: #000;
+  -webkit-text-fill-color: transparent;
+  -webkit-text-stroke-width: 1px;
+}
+</style>
---
+++ src/components/CreateTodo.vue
@@ -0,0 +1,49 @@
+<script></script>
+
+<template>
+  <div class="todo-box">
+    <input
+      class="todo-input"
+      type="text"
+      placeholder="I'm gonna finish my homework."
+    />
+    <button class="todo-button">+</button>
+  </div>
+</template>
+
+<style scoped>
+.todo-box {
+  display: flex;
+  width: 100%;
+  max-width: 680px;
+  border: solid 1px #000;
+
+  padding: 16px;
+  gap: 12px;
+}
+
+.todo-input {
+  width: 100%;
+  border: solid 1px #000;
+  background-color: transparent;
+  padding: 8px;
+}
+
+.todo-button {
+  cursor: cell;
+  background-color: transparent;
+
+  color: black;
+  font-size: 16px;
+  font-weight: 600;
+
+  border: solid 1px #000;
+  padding: 8px 12px;
+
+  transition: all 150ms ease;
+}
+
+.todo-button:hover {
+  background-color: var(--primary-color);
+}
+</style>
